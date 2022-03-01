43260149084c9b28f565ad082dfe68fe1771c694
root <zacharias.boehler@gmail.com>
2022-03-01 18:24:57 +0100

    added files twice

--- imgs/test.go
+++
@@ -1,9 +0,0 @@
-package main
-
-import (
-    "fmt"
-)
-func main() {
-    v := 394
-    fmt.Println("./ipfs","config","--json","Addresses.Swarm",fmt.Sprintf("'[\"/ip4/0.0.0.0/tcp/%d\",\"/ip6/::/tcp/%d\",\"/ip4/0.0.0.0/udp/%d/quic\",\"/ip6/::/udp/%d/quic\"]'",v,v,v,v))
-}
